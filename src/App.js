b1f1d761d2f32bb3506523246d0155bca814bbd5
fun_actuator <kuldeep.msvm@gmail.com>
2023-01-29 15:36:33 +0530

    layout and dashlayout done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
+import DashLayout from "./components/DashLayout";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Public/>} />
+        <Route path="login" element={<Login/>} />
+        <Route path="dash" element={<DashLayout/>}/>
+        
+        <Route/>
+      </Route>
+    </Routes>
   );
 }
 
